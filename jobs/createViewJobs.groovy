a886b30f3da86fa324642708c46e4b4aaade7422
Serge Starikov <sergey.starikov@starikov-mac.local>
2017-01-20 11:17:07 +0200

    added AL init build job

--- jobs/createViewJobs.groovy
+++ jobs/createViewJobs.groovy
@@ -19,3 +19,45 @@ dashboardView('backend') {
         testTrendChart()
     }
 }
+dashboardView('frontend') {
+    jobs {
+        regex(/gc-.*/)
+    }
+    columns {
+        status()
+        weather()
+        buildButton()
+    }
+    topPortlets {
+        jenkinsJobsList {
+            displayName('frontend jobs')
+        }
+    }
+    leftPortlets {
+        testStatisticsChart()
+    }
+    rightPortlets {
+        testTrendChart()
+    }
+}
+dashboardView('seedJob') {
+    jobs {
+        regex(/seed.*/)
+    }
+    columns {
+        status()
+        weather()
+        buildButton()
+    }
+    topPortlets {
+        jenkinsJobsList {
+            displayName('seed job')
+        }
+    }
+    leftPortlets {
+        testStatisticsChart()
+    }
+    rightPortlets {
+        testTrendChart()
+    }
+}
